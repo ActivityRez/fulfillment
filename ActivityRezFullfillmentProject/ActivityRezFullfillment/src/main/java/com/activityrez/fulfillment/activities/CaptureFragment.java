39c2875b08cf47ba8d1fa04d650bf743bcc17b63
hiro <hiro@hawaiioceanline.com>
2014-05-22 11:15:02 -1000

    cleaned up a bit

--- ActivityRezFullfillmentProject/ActivityRezFullfillment/src/main/java/com/activityrez/fulfillment/activities/CaptureFragment.java
+++ ActivityRezFullfillmentProject/ActivityRezFullfillment/src/main/java/com/activityrez/fulfillment/activities/CaptureFragment.java
@@ -40,7 +40,7 @@ public class CaptureFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-        ARContainer.bus.register(this);
+//        ARContainer.bus.register(this);
         try {
             cameraManager.openDriver();
         } catch(IOException e){}
@@ -63,6 +63,6 @@ public class CaptureFragment extends Fragment {
         super.onPause();
         cameraManager.stopPreview();
         cameraManager.closeDriver();
-        ARContainer.bus.unregister(this);
+//        ARContainer.bus.unregister(this);
     }
 }
